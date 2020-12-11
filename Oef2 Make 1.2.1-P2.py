e9c9699a8f9774a1d8b6da84226c7dfa1c9a7fcd
JasperWithofs <withofs.jasper@gmail.com>
2020-12-11 15:08:30 +0100

    versie 1.0.1

--- Oef2 Make 1.2.1-P2.py
+++ Oef2 Make 1.2.1-P2.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
 
-            
+#  Description
+"""A program that checks if your number is even """
 # Futures
-
 # Libs
 
 __author__ = "Jasper Withofs"
 __email__ = "jasper.withofs@student.kdg.be"
 __version__ = "1.0.1"
-__status__ = "Development" 
+__status__ = "Development"
+
+num = int(input("enter a number: "))
+
+if (num % 2) == 0:
+    print(num, " is even ")
+else:
+    print(num, " is odd")
